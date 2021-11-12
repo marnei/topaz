a0270b981a18fefd93279d6709546d64cc94e814
marnei <marnei@digistar.com.br>
2021-11-12 14:06:20 -0300

    Final version

--- load_balance.py
+++ load_balance.py
@@ -1,4 +1,21 @@
-"""load_balance.py  Responsable for load balance users in servers"""
+""""
+    load_balance.py  Responsable for load balance users in servers
+"""
+
+SERVER_MAX_TICKS = 10
+SERVER_MIN_TICKS = 1
+SERVER_MAX_USERS = 10
+SERVER_MIN_USERS = 1
+SERVER_TICK_COST = 1
+
+
+def get_server_borders(num_ticks, max_users):
+    """Tests if max_users borders"""
+
+    if num_ticks < SERVER_MIN_TICKS or num_ticks > SERVER_MAX_TICKS \
+            or max_users < SERVER_MIN_USERS or max_users > SERVER_MAX_USERS:
+        return False
+    return True
 
 
 class Balance:
@@ -15,37 +32,85 @@ class Balance:
         self.ticks_count = 1
         self.cost = 0
         self.servers_list = []
+        self.users_list = []
 
     def add_new_users(self, users):
         """
             Add users to user list. Each user is allocated in a server
         """
 
-        self.ticks_count = self.ticks_users # number of ticks to complete all tasks at end of users list
-        for i in range(users): # Add each user to low cost server.
-            self.servers_list.append(self.get_best_server())
+        self.ticks_count = self.ticks_users
+        # number of ticks to complete all tasks at end of users list
+        for i in range(users):  # Add each user to low cost server.
+            server = self.get_best_server()
+            user = User(server, self.ticks_users)
+            user.server.users = user.server.users + 1
+            self.users_list.append(user)
 
     def do_tick(self):
-        self.cost = self.cost + 1
+        """
+            For each tick decrement users task ticks count.
+            If task done, remove user from server. If servers users count
+            is null, then remove server.
+
+            return tick servers info.
+        """
+
+        copy_users_list = self.users_list.copy()
+        for user in copy_users_list:
+            if user.task_tick == 0:
+                user.server.users = user.server.users - 1
+                if user.server.users == 0:
+                    self.servers_list.remove(user.server)
+                self.users_list.remove(user)
+            user.task_tick = user.task_tick - 1
+        self.cost = self.cost + (SERVER_TICK_COST * len(self.servers_list))
+        if self.servers_list:
+            tick_server_info = ''
+            for index, server in enumerate(self.servers_list):
+                if index == 0:
+                    tick_server_info = str(server.users)
+                else:
+                    tick_server_info = tick_server_info + ','\
+                                       + str(server.users)
+                server.tick_counter = server.tick_counter + 1
+        else:
+            tick_server_info = '0'
+        print('servers=', tick_server_info)
+        return tick_server_info
 
     def get_best_server(self):
         """
-            Looking for best server. Best server is not full with more number of users.
+            Looking for best server.
+            Best server is not full with more number
+            of users or a new one if all is full.
         """
 
         best_server = None
         if self.servers_list:
             for server in self.servers_list:
                 if server.users < self.max_users:
-                    if server.users+1 == self.max_users: # If this server is full with one more user this is almost full so, is the best
-                        return server
-                    if best_server and best_server.users < server.users:
-                        best_server = server
+                    if best_server:
+                        if best_server.users < server.users:
+                            # Number of users is greater.
+                            best_server = server
+                        if best_server.users == server.users \
+                                and \
+                                best_server.tick_counter > server.tick_counter:
+                            # Number of users is same
+                            # but this server is newer.
+                            best_server = server
                     if not best_server:
                         best_server = server
-        if best_server is None:
+        if best_server is None:  # All servers full or no server on list
             best_server = Server()
+            self.servers_list.append(best_server)
         return best_server
+
+    def get_cost(self):
+        """return cost to print"""
+
+        return str(self.cost)
 
 
 class Server:
@@ -55,6 +120,7 @@ class Server:
 
     def __init__(self):
         self.users = 0
+        self.tick_counter = 0
 
 
 class User:
@@ -62,4 +128,3 @@ class User:
     def __init__(self, server, task_tick):
         self.server = server
         self.task_tick = task_tick
-
--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-"""Main program for load balance in servers use.
 """
+    Main program for load balance in servers use.
+
+    USAGE: python3 main.py <input.txt> <output.tx>
+"""
+
 
 import sys
 import load_balance
-import servers
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     """
         sys.argv[1] is file input
         sys.argv[2] is file output
-        
-        Read from file input: 
-        Line 1 number of ticks per user task
-        Line 2 number of users allowed per server 
-        Other lines number of new users per tick
-        
-        Return in file output:
-        In each line the number of current users in each tick per server. Each server separated by a comma
-        The last represent cost. Cost is the sum of each tick.
-        
-    """
 
-    file_input = open(sys.argv[1], 'r')
+        Read from file input:
+        Line 1 number of ticks per user task
+        Line 2 number of users allowed per server
+        Other lines number of new users per tick
+
+        Return in file output:
+        In each line the number of current users for server,
+        each server separated by a comma.
+        The line last represent cost. Cost is the sum of each line cost.
+
+"""
+
     try:
+        file_input = open(sys.argv[1], 'r')
+        open(sys.argv[2], 'w').close()
+        # Cleaning output file
+        file_output = open(sys.argv[2], 'w')
         num_ticks_task = int(file_input.readline())
         max_server_users = int(file_input.readline())
-        if not servers.get_server_borders(num_ticks_task, max_server_users):
+        if not load_balance.get_server_borders(num_ticks_task, max_server_users):
             sys.exit('ERROR: Wrong server file parameter border')
     except Exception as erro:
         sys.exit('ERROR:{}'.format(erro))
@@ -35,18 +41,21 @@ if __name__ == '__main__':
     while load_balance.ticks_count:
         users_to_add = file_input.readlines()
         for num_users in users_to_add:
-            load_balance.do_tick()
             try:
                 num_users_int = int(num_users)
                 if num_users_int:
                     load_balance.add_new_users(num_users_int)
                 else:
                     if load_balance.ticks_count > 1:
-                      load_balance.ticks_count = load_balance.ticks_count - 1
+                        load_balance.ticks_count = load_balance.ticks_count - 1
+                line = load_balance.do_tick()
+                file_output.write(line + '\n')
             except Exception as erro:
                 print('ERROR:{}'.format(erro))
-        load_balance.do_tick()
+        line = load_balance.do_tick()
+        file_output.write(line + '\n')
         load_balance.ticks_count = load_balance.ticks_count - 1
-    print('tick=', load_balance.cost)
-
-
+    file_output.write(load_balance.get_cost() + '\n')
+    file_output.close()
+    file_input.close()
+    sys.exit('DONE')
